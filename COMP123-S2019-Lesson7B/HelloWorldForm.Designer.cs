7967477a0e18fa9cbd9ad79de720f33812edc0ca
300987900 <300987900@PR-E216-27>
2019-06-26 15:24:23 -0400

    added functionality message

---
+++ COMP123-S2019-Lesson7B/HelloWorldForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace COMP123_S2019_Lesson7B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloWorldlabel = new System.Windows.Forms.Label();
+            this.ClickMebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HelloWorldlabel
+            // 
+            this.HelloWorldlabel.BackColor = System.Drawing.Color.White;
+            this.HelloWorldlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HelloWorldlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelloWorldlabel.Location = new System.Drawing.Point(235, 225);
+            this.HelloWorldlabel.Name = "HelloWorldlabel";
+            this.HelloWorldlabel.Size = new System.Drawing.Size(187, 43);
+            this.HelloWorldlabel.TabIndex = 0;
+            this.HelloWorldlabel.Text = "Hello,World!!!";
+            // 
+            // ClickMebutton
+            // 
+            this.ClickMebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClickMebutton.Location = new System.Drawing.Point(74, 225);
+            this.ClickMebutton.Name = "ClickMebutton";
+            this.ClickMebutton.Size = new System.Drawing.Size(142, 43);
+            this.ClickMebutton.TabIndex = 1;
+            this.ClickMebutton.Text = "Click Me!";
+            this.ClickMebutton.UseVisualStyleBackColor = true;
+            this.ClickMebutton.Click += new System.EventHandler(this.ClickMebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.ClickMebutton);
+            this.Controls.Add(this.HelloWorldlabel);
+            this.Name = "Form1";
+            this.Text = "HelloWorld";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloWorldlabel;
+        private System.Windows.Forms.Button ClickMebutton;
+    }
+}
+
---
+++ COMP123-S2019-Lesson7B/HelloWorldForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lesson7B
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// this.is the HelloWorld Constructor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+        /// <summary>
+        /// The start method is used for property initialization
+        /// </summary>
+        public void Start()
+        {
+            HelloWorldlabel.BackColor = Color.Black;
+            HelloWorldlabel.ForeColor = Color.White;
+            HelloWorldlabel.Text = "Hellow, Pluto";
+        }
+        /// <summary>
+        /// This method is the event hardler for the ClickMeButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClickMebutton_Click(object sender, EventArgs e)
+        {
+            //if (HelloWorldlabel.Text == "Clicked!")
+            //{
+            //    HelloWorldlabel.Text = "UnClicked";
+            //}
+            //else
+            //{
+            //    HelloWorldlabel.Text = "Clicked!";
+            //}
+            HelloWorldlabel.Text = (HelloWorldlabel.Text == "Clicked!") ? "Unclicked!" : "Clicked!";
+
+            int x = new Random().Next(1,49);
+            HelloWorldlabel.Text =x.ToString();
+                
+        }
+    }
+}
